5a7c25c8a1b2413da46091ffb09e21ab6ea9a731
Mohamed Nagdy <mohamednagdy257@gmail.com>
2022-11-10 02:06:48 +0200

    fix some issues

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,2 @@
 ## [1.0.0] - First release which contains every dialogs and all functionality 
+## [1.0.1] - change some features 
--- lib/animations/animations.dart
+++ lib/animations/animations.dart
@@ -1,17 +1,15 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:vector_math/vector_math.dart' as vectormath;
 
 /// the animation for the dialog
 /// we will set the scale animation as default animation
 class DialogAnimations {
-
-  static double getCurvedValue(animation){
+  static double getCurvedValue(animation) {
     return Curves.easeInOutCirc.transform(animation.value) - 1.0;
   }
 
   /// scale the dialog from zero size to the max height and width
   static Transform scaleDialog({
-
     /// the child
     required Widget child,
 
@@ -30,14 +28,12 @@ class DialogAnimations {
 
   /// rotate dialog 360 degree animation
   static Transform rotateDialog({
-
     /// the child
     required Widget child,
 
     /// the animation as double
     required Animation<double> animation,
   }) {
-
     // rotate the child
     return Transform.rotate(
       // using vector math library to get the angle as 360 degree and
@@ -50,20 +46,18 @@ class DialogAnimations {
     );
   }
 
-
   /// animate the dialog from up to down
   static Transform upToDown({
-
     /// the child
     required Widget child,
 
     /// the animation as double
     required Animation<double> animation,
   }) {
-
     return Transform(
       // define the matrix 4 translation from our curve
-      transform: Matrix4.translationValues(0.0, getCurvedValue(animation) * 200, 0.0),
+      transform:
+          Matrix4.translationValues(0.0, getCurvedValue(animation) * 200, 0.0),
       child: Opacity(
         opacity: animation.value,
         child: child,
@@ -73,7 +67,6 @@ class DialogAnimations {
 
   /// animate the dialog from bottom to up
   static Transform downToUp({
-
     /// the child
     required Widget child,
 
@@ -82,7 +75,8 @@ class DialogAnimations {
   }) {
     return Transform(
       // define the matrix 4 translation from our curve
-      transform: Matrix4.translationValues(0.0, getCurvedValue(animation) * -200, 0.0),
+      transform:
+          Matrix4.translationValues(0.0, getCurvedValue(animation) * -200, 0.0),
       child: Opacity(
         opacity: animation.value,
         child: child,
@@ -92,17 +86,16 @@ class DialogAnimations {
 
   /// animate the dialog from left to right
   static Transform leftToRight({
-
     /// the child
     required Widget child,
 
     /// the animation as double
     required Animation<double> animation,
   }) {
-
     return Transform(
       // define the matrix 4 translation from our curve
-      transform: Matrix4.translationValues(getCurvedValue(animation) * 200, 0.0, 0.0),
+      transform:
+          Matrix4.translationValues(getCurvedValue(animation) * 200, 0.0, 0.0),
       child: Opacity(
         opacity: animation.value,
         child: child,
@@ -112,7 +105,6 @@ class DialogAnimations {
 
   /// animate the dialog from right to left
   static Transform rightToLeft({
-
     /// the child
     required Widget child,
 
@@ -121,7 +113,8 @@ class DialogAnimations {
   }) {
     return Transform(
       // define the matrix 4 translation from our curve
-      transform: Matrix4.translationValues(getCurvedValue(animation) * -200, 0, 0),
+      transform:
+          Matrix4.translationValues(getCurvedValue(animation) * -200, 0, 0),
       child: Opacity(
         opacity: animation.value,
         child: child,
