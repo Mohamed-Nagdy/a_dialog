7e1e3df900d99bb91a2b87b4eb178069d16184f3
Brett Sutton <bsutton@noojee.com.au>
2023-09-13 20:32:53 +1000

    Released 1.1.0.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,7 @@
+# 1.1.0
+ - Fixed the pubspec.yaml asset delcaration. It was missing the trailing slash
+  which resulted in the assets being unavailable when a_dialog is used as a dependency.
+  https://github.com/flutter/flutter/issues/134527
+
 ## [1.0.0] - First release which contains every dialogs and all functionality 
 ## [1.0.1] - change some features 
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,13 +3,15 @@ import 'package:a_dialog/utils/a_dialog_controller.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Test",
       home: TestPage(),
